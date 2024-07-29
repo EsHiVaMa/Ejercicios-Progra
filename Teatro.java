ff8fb63410dada641f1d5a4420a5c81abbdbffb1
Esteban Vargas <var24530@uvg.edu.gt>
2024-07-28 22:21:06 -0600

    Update 2 Teatro.java

--- Teatro.java
+++ Teatro.java
@@ -4,6 +4,8 @@ private int Capacidad;
 private Localidad balcon1;
 private Localidad balcon2;
 private Localidad platea;
+private int totalTickvent;
+private Tickets tick;
 
     //Constructores
 
@@ -13,13 +15,17 @@ this.Capacidad = 0;
 this.balcon1= new Localidad();
 this.balcon2=new Localidad();
 this.platea=new Localidad(); 
+this.totalTickvent=0;
+this.tick=new Tickets();
 }
-public Teatro(String Nombre, int Capacidad, Localidad balcon1, Localidad balcon2, Localidad platea){
+public Teatro(String Nombre, int Capacidad, Localidad balcon1, Localidad balcon2, Localidad platea,int totalTickvent,Tickets tick){
     this.Nombre = Nombre;
     this.Capacidad=Capacidad;
     this.balcon1=balcon1;
     this.balcon2=balcon2;
     this.platea=platea;
+    this.totalTickvent=totalTickvent;
+    this.tick=tick;
     }
 
     //sets y gets
@@ -28,10 +34,27 @@ public int calculardisp(){
 int total = balcon1.getCapacidad()+balcon2.getCapacidad()+platea.getCapacidad();
 this.Capacidad = total; 
 return total; 
-} 
-//Calcula y retorna la cantidad de asientos disponibles en todo el teatro.
+} //Calcula y retorna la cantidad de asientos disponibles en todo el teatro.
+public int getCapacidad(){
+    return Capacidad;
+}
 public int Actudisp(){
-int cbalcon1= balcon1.getCapacidad(); 
+int ticketstol = tick.getcantBol();
+int ubi = tick.tubicación();
+int total = this.Capacidad;  
+int cbalcon1= balcon1.getCapacidad();
+int cbalcon2=balcon2.getCapacidad();
+int cplatea=platea.getCapacidad(); 
+if(ubi==1){
+cbalcon1=cbalcon1-ticketstol;
+}
+if(ubi==2){
+    cbalcon2=cbalcon2-ticketstol;
+}
+if(ubi==3){
+    cplatea=cplatea-ticketstol;
+}
+return (this.Capacidad=total);
 }
 
 }

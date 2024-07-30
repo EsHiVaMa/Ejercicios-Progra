8781926ec56670591a8e8b9f0c8ed0720b0d9a4b
Esteban Vargas <var24530@uvg.edu.gt>
2024-07-29 22:52:05 -0600

    Update Teatro y.java

--- Teatro.java
+++ Teatro.java
@@ -4,27 +4,22 @@ private int Capacidad;
 private Localidad balcon1;
 private Localidad balcon2;
 private Localidad platea;
-private int totalTickvent;
 private Tickets tick;
-
-    //Constructores
-
+//Constructores
     public Teatro(){
 this.Nombre = new String();
 this.Capacidad = 0;
 this.balcon1= new Localidad();
 this.balcon2=new Localidad();
 this.platea=new Localidad(); 
-this.totalTickvent=0;
 this.tick=new Tickets();
 }
-public Teatro(String Nombre, int Capacidad, Localidad balcon1, Localidad balcon2, Localidad platea,int totalTickvent,Tickets tick){
+public Teatro(String Nombre, int Capacidad, Localidad balcon1, Localidad balcon2, Localidad platea,Tickets tick){
     this.Nombre = Nombre;
     this.Capacidad=Capacidad;
     this.balcon1=balcon1;
     this.balcon2=balcon2;
     this.platea=platea;
-    this.totalTickvent=totalTickvent;
     this.tick=tick;
     }
 
@@ -32,9 +27,20 @@ public Teatro(String Nombre, int Capacidad, Localidad balcon1, Localidad balcon2
     //Funciones
 public int calculardisp(){
 int total = balcon1.getCapacidad()+balcon2.getCapacidad()+platea.getCapacidad();
-this.Capacidad = total; 
-return total; 
+return (this.Capacidad = total); 
 } //Calcula y retorna la cantidad de asientos disponibles en todo el teatro.
+
+
+public Localidad getbalcon1(){
+    return balcon1;
+}
+public Localidad getbalcon2(){
+    return balcon2;
+}
+public Localidad getplatea(){
+    return platea;
+}
+
 public int getCapacidad(){
     return Capacidad;
 }
@@ -55,6 +61,6 @@ if(ubi==3){
     cplatea=cplatea-ticketstol;
 }
 return (this.Capacidad=total);
-}
+}//Actualiza la disponibilidad de asientos en la localidad seleccionada.
 
 }

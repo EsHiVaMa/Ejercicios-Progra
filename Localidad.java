55d3ee18fb4adcf2aabb75a4255d845c04d51d07
Esteban Vargas <var24530@uvg.edu.gt>
2024-07-29 22:52:58 -0600

    Update Localidad y.java

--- Localidad.java
+++ Localidad.java
@@ -2,6 +2,14 @@ public class Localidad {
     private int Capacidad;
     private float Precio;
 
+    public float getPrecio() {
+        return this.Precio;
+    }
+
+    public void setPrecio(float Precio) {
+        this.Precio = Precio;
+    }
+
     //Constructores
 
     public Localidad(){
@@ -16,6 +24,9 @@ public Localidad(int Capacidad, float Precio){
     //sets y gets
 public int getCapacidad(){
     return Capacidad;
+}
+public int setCapacidad(int Capacidad){
+    return (this.Capacidad=Capacidad);
 }
 
 }
